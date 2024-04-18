1fa5a2b60b98cee06ed5ab6f164840e1ae69c6d3
adharsh-A <103622525+adharsh-A@users.noreply.github.com>
2024-04-18 21:19:17 +0530

    after responsive

--- 4.Password-Generator-main/script.js
+++ 4.Password-Generator-main/script.js
@@ -9,11 +9,11 @@ const allChars = upperCase + lowerCase + number + symbol;
 
 function createPassword(){
     let password = "";
-    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    /* password += upperCase[Math.floor(Math.random() * upperCase.length)];
     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
     password += number[Math.floor(Math.random() * number.length)];
     password += symbol[Math.floor(Math.random() * symbol.length)];
-
+ */
     while(lenght>password.length){
         password += allChars[Math.floor(Math.random() * allChars.length)];
     }
